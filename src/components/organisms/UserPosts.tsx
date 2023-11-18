366a50e26aa18803047bcde21ac6b211d9c35a72
Ephraim Jan Conales <conalesej@gmail.com>
2023-11-18 23:20:31 +0800

    UserPosts - first commit

---
+++ src/components/organisms/UserPosts.tsx
@@ -0,0 +1,48 @@
+import { Box, Divider, Flex, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetPostsByUserIdQuery } from "../../features/post/api";
+import { PostComponent as Post } from "../atoms";
+
+interface IUserPosts {}
+const UserPosts: React.FC<IUserPosts> = () => {
+  const { id } = useParams();
+
+  const {
+    data: posts,
+    isLoading,
+    isSuccess,
+  } = useGetPostsByUserIdQuery(id as string);
+
+  if (isLoading) {
+    return <div>...Loading</div>;
+  }
+
+  return (
+    <Box margin={"20px auto 0px"} width={"95%"}>
+      <Text fontSize={"xx-large"} as={"b"}>
+        {id}'s Posts 📃
+      </Text>
+      <Divider mt={5} color={"black"} opacity={1} />
+
+      <Flex
+        flexDirection={"column"}
+        maxWidth={"100%"}
+        width={"50%"}
+        gap={2}
+        margin={"20px auto"}
+      >
+        {posts?.map((post) => (
+          <Stack
+            key={post.id}
+            className="border border-gray-300 hover:z-10 hover:border-blue-500 focus:border-blue-500 hover:shadow-outline-blue focus:shadow-outline-blue transform hover:scale-105 focus:scale-105 transition duration-300 ease-in-out "
+          >
+            <Post post={post} />
+          </Stack>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserPosts;
