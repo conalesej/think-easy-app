39ace103dc0c5460e1f696bb363b3e09dbbacbe8
Ephraim Jan Conales <conalesej@gmail.com>
2023-11-28 12:27:18 +0800

    Main - remove protection route for /

--- src/main.tsx
+++ src/main.tsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter(
       path: "/",
       element: (
         <Layout>
-          <ProtectedRoute element={<App />} />
+          <App />
         </Layout>
       ),
       errorElement: (
